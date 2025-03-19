03700ac9c6b6df1200a8beea120e16e02cd1e56d
Kobby-Jones <kobbyj099@gmail.com>
2025-03-19 05:26:34 +0000

    Updated Projects

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const visitorCount = document.getElementById('visitor-count')
+const contactForm = document.getElementById('contact-form')
+const person_name = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
 
 async function getVisitorCount() {
     try{
@@ -11,5 +15,17 @@ async function getVisitorCount() {
       }
   
 }
-
 getVisitorCount()
+
+async function onSubmit(){
+    const response = await fetch("https://iubobq9vhj.execute-api.us-west-2.amazonaws.com/dev/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ person_name, email, message })
+    });
+
+    const result = await response.json();
+    alert(result.message);
+
+}
+
